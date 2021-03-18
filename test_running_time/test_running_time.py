1d9976a219b85349dff3cceb55627286d7e8410d
Pierre Ung <ung@etud.insa-toulouse.fr>
2021-03-18 17:03:02 +0100

    Changement script test (Expected / Result affiché)

--- test_running_time/test_running_time.py
+++ test_running_time/test_running_time.py
@@ -21,12 +21,15 @@ def test_speed(path_grammaire, path_words):
         exit()
     
     words = content.splitlines()
+    
 
-    for word in words:                                            # Boucle testant le temps d'execution des mots passé en argument
-        print("\ntest word : "+word)
+    for word in words:
+        word = word.split(':')                                            # Boucle testant le temps d'execution des mots passé en argument
+        print("\ntest word : "+word[0])
+        print("Expected :" + word[1] + "\nResult : ")
         timeStarted = time.time()                                 # Sauvegarde du temps de départ
 
-        subprocess.run(["./parserToTest",word])                  # Execution du parser avec sur un mot
+        subprocess.run(["./parserToTest",word[0]])                  # Execution du parser avec sur un mot
 
         timeDelta = time.time() - timeStarted                     # Temps d'execution
         print("Finished process in "+str(timeDelta)+" seconds.") 
@@ -46,3 +49,9 @@ test_speed("./grammaires/g_test2.txt","./grammaires/words/g_test2_words.txt")
 # Grammaire 3
 #words = ["x x x y y y","x x y","x x x x x a a b b b b y y y y y",""]
 test_speed("./grammaires/g_test3.txt","./grammaires/words/g_test3_words.txt")
+
+# Grammaire 4
+test_speed("./grammaires/g_test4.txt","./grammaires/words/g_test4_words.txt")
+
+# Grammaire 5
+test_speed("./grammaires/g_test5.txt","./grammaires/words/g_test5_words.txt")
